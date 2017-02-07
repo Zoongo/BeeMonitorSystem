db6a88f0568b6f9197235dd19fb8002f600b2d3d
Zoongo <ivanskyt@zoongos.com>
2017-02-07 12:43:56 +0100

    Update BeeMonitorRxArduinoWifi.ino

--- Receiver/BeeMonitorRxArduinoWifi/BeeMonitorRxArduinoWifi.ino
+++ Receiver/BeeMonitorRxArduinoWifi/BeeMonitorRxArduinoWifi.ino
@@ -1,8 +1,22 @@
 //--------------------------------------------
 //--- BeeMonitor receiver
-//--- Version for: 
-//--- Arduino Mega and Arduino Wifi shield
-//--- XBee Series1
+//--- Board: Arduino Mega
+//--- Communication: XBee Series1 + WiFi
+//--------------------------------------------
+// Copyright (C) 2015-2017 by Tomas Ivansky
+//--------------------------------------------
+// GNU GPL v3 License
+// ------------------
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// The license can be viewed at
+// http://www.gnu.org/licenses/gpl-3.0.txt
 //--------------------------------------------
 
 #include <WiFi.h>
